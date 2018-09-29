0c2b817abd36d21f37194a22529aa65a8a38a31a
Koen Punt <koen@koenpunt.nl>
2018-09-29 23:32:00 +0200

    fix typos in directive template

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const { SchemaDirectiveVisitor } = require('graphql-tools');
 
-class TempateDirective extends SchemaDirectiveVisitor {
+class TemplateDirective extends SchemaDirectiveVisitor {
   //
   // Check this article for implementation details:
   // https://dev-blog.apollodata.com/reusable-graphql-schema-directives-131fb3a177d1
   //
-  // also exck out SchemaDirective docs from graphql-tools
+  // also check out the SchemaDirective docs from graphql-tools
   // https://www.apollographql.com/docs/graphql-tools/schema-directives.html
   //
 }
 
-module.exports = templateDirective;
+module.exports = TemplateDirective;
